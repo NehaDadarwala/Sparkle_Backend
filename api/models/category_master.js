5a3ee8d3a1a35102b8df019022f7062e91d9db19
sakshi-124 <vaidyasakshi434@gmail.com>
2023-03-28 14:10:25 -0300

    inventory damnn

--- api/models/category_master.js
+++ api/models/category_master.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 const categoryMasterSchema = new mongoose.Schema({
+    category_name: {
+        type: String,
+        required: true
+    },
+    short_name: {
+        type: String,
+        required : true
+    }
 }, {
     collection: 'category_master'
 });
--- api/routes/inventory.js
+++ api/routes/inventory.js
@@ -5,14 +5,14 @@ const CategoryMaster = require('../models/category_master');
 const ProductMaster = require('../models/product_master');
 
 const Storage = multer.diskStorage({
-    destination : 'uploads',
-    filename : (req,file,cb) => {
-        cb(null,file.originalname);
+    destination: 'images',
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
     }
 })
 
 const upload = multer({
-    storage : Storage
+    storage: Storage
 });
 
 
@@ -20,24 +20,33 @@ router.get('/', async (req, res) => {
     res.json({ message: "Basic Invetory API" })
 })
 
-function generateProductRefNumber() //product_name , product_category
+
+// ProductMaster.aggregate([
+//     {
+//         $lookup: {
+//             from: 'category_master',
+//             localField: 'category_id',
+//             foreignField: '_id',
+//             as: 'category'
+//         }
+//     }
+// ]);
+
+async function generateProductRefNumber(category_id)
 {
-    const results = ProductMaster.aggregate([
-        {
-          $lookup: {
-            from: 'category_master',
-            localField: 'category_id',
-            foreignField: '_id',
-            as: 'category'
-          }
-        }
-      ]);
-      console.log(results);
-    const product_ref_number ="RN-1";
+
+    const categoryMaster = await CategoryMaster.findById(category_id)
+    const cate_id = { "category_id": category_id }
+    console.log(categoryMaster)
+    const shortName = categoryMaster.short_name
+    console.log(shortName)
+    const maxNumber = await ProductMaster.countDocuments(cate_id) + 1 ;
+    const product_ref_number = shortName + '-' + maxNumber
+    console.log(product_ref_number)
     return product_ref_number;
 }
 
-router.get('/category', async(req,res)  =>{
+router.get('/category', async (req, res) => {
     CategoryMaster.find({}).then(data => {
         res.status(200).json({
             message: "Category Retrived",
@@ -53,18 +62,21 @@ router.get('/category', async(req,res)  =>{
 });
 
 router.post('/addProduct', upload.single("image"), async (req, res) => {
+    const ref_number = await generateProductRefNumber(req.body.category_id);
+    console.log(ref_number)
+
     const newProduct = new ProductMaster({
         product_name: req.body.product_name,
         category_id: req.body.category_id,
         qty: req.body.qty,
         price: req.body.price,
         product_description: req.body.product_description,
-        product_ref_number:generateProductRefNumber(),
+        product_ref_number: ref_number,
         image: {
-            data : req.file.filename,
-            contentType:'image/png '
+            data: req.file.filename,
+            contentType: 'image/png '
         }
-    }); 
+    });
     await newProduct.save()
         .then((data) => {
             res.status(200).json({
@@ -81,7 +93,7 @@ router.post('/addProduct', upload.single("image"), async (req, res) => {
         });
 })
 
-router.get('/viewStock',async(req,res) =>{
+router.get('/viewStock', async (req, res) => {
     ProductMaster.find({}).then(data => {
         res.status(200).json({
             message: "Stock Retrived",
