05b2b75891ebd86ce01388ca40fc58240f98ae4e
sakshi-124 <vaidyasakshi434@gmail.com>
2023-03-28 14:22:47 -0300

    sol-2

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.error('Connected to the database'))
 
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(cors())
 
 const refundRouter = require('./api/routes/refund')
