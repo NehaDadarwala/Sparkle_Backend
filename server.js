4669295149cdfcdc508dfcc9f2eabf5ce679fd86
sakshi-124 <vaidyasakshi434@gmail.com>
2023-03-28 14:25:48 -0300

    sol-3

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-app.use(cors())
+  app.use(cors({
+    origin: '*'
+  }));
 
 const refundRouter = require('./api/routes/refund')
 app.use('/refund', refundRouter)
