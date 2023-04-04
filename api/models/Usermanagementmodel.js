fe1cf312fbdf8af965cd808b32d6e6898d42407d
Hargun-Chhabra <47248878+Hargun-Chhabra@users.noreply.github.com>
2023-04-04 19:24:14 -0300

    Update Usermanagementmodel.js

--- api/models/Usermanagementmodel.js
+++ api/models/Usermanagementmodel.js
@@ -1,3 +1,4 @@
+// This code is an adpatation of code provided in the gitrepo: https://github.com/CodeWithHarry/iNotebook-React/blob/master/backend/models/User.js provided by codewitharry
 const mongoose=require('mongoose');
 
 const userschema = new mongoose.Schema({
